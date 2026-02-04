ede5cf3084206888eda254c9043421a3e4831b21
Jayesh Lokhande <jayeshlokhande@Jayeshs-MacBook-Pro.local>
2026-02-04 14:03:18 +0530

    Hardcode backend URL to fix connection

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,7 @@
 // If we are in production (live), use the live backend URL (we will set this later).
 // If we are developing locally, use localhost:8000.
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+// export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+
+
+// client/src/config.js
+export const API_URL = "https://clinicsen-app.onrender.com";
